4d95e8beab521ca31995c3745de46bef09671029
Mihail <mihail.krasun@mail.ru>
2022-01-22 20:50:44 +0400

    feat: add 2 routes and tested

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 '''
     Main app
 '''
+
+from bottle import route, run, template
+
+
+@route("/")
+def main():
+    with open("index.html", mode="r") as index_file:
+        index_data = index_file.read()
+        return index_data
+
+@route('/<filename>')
+def index(filename):
+    with open(filename, mode="r") as index_file:
+        index_data = index_file.read()
+        return index_data
+
+run(host='localhost', port=8080)
