3c8dd82aa37eaa659604a26464b24c3acf291b3c
Mihail <mihail.krasun@mail.ru>
2022-01-22 21:24:04 +0400

    fix: add documents in readme

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def main():
 def index(filename):
     with open(filename, mode="r") as index_file:
         index_data = index_file.read()
-        return index_data
+        return index_data   
 
 run(host='localhost', port=8080)
